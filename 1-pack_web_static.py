a6784f415fe1bcac64b20e26cafc9b5eb9b37f3e
Iwamgad <dagmawiharry2@gmail.com>
2022-10-27 06:07:40 -0700

    I edited some files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# A fabric python script that generates a .tgz archive from the contents of the web_static folder
+# A fabric python script that generates a .tgz archive from the contents of
+# the web_static folder
 
 from datetime import datetime
 from fabric.api import *
@@ -8,8 +9,7 @@ from fabric.api import *
 def do_pack():
     """This method creates a tar archive of the directory web_static"""
     date = datetime.now()
-    archivePath = "versions/web_static_" + date.strftime("%Y%m%d%H%M%S") + ".tgz"
-
+    archivePath = "versions/web_static_" +date.strftime("%Y%m%d%H%M%S") + ".tgz"
     local("mkdir -p versions")
     createdFile = local("tar -cvzf {} web_static".format(archivePath))
 
